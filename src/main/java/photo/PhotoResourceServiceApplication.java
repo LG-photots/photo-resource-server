995ac903e0e34d7379281fa75d3a1070fc9cbbc1
Lwazi <lwazigxogxa@gmail.com>
2023-07-23 10:40:33 +0200

    first commit

---
+++ src/main/java/photo/PhotoResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoResourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoResourceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/photo/PhotoResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
