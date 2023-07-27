ec9bd799c1ec842d008cfb13b597d04ac2d9fc2c
Lwazi <lwazigxogxa@gmail.com>
2023-07-27 16:52:45 +0200

    role based access

--- src/main/java/photo/controller/UsersController.java
+++ src/main/java/photo/controller/UsersController.java
@@ -1,10 +1,11 @@
 package photo.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import photo.dto.UserDto;
 
 @RestController
 @RequestMapping("/users")
@@ -18,5 +19,20 @@ public class UsersController {
     @GetMapping("/jwt")
     public Jwt getJwt(@AuthenticationPrincipal Jwt jwt){
         return jwt;
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+    //@Secured("ROLE_developer")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
+
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserDto getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        String f = jwt.getSubject();
+        return new UserDto("Sergey", "Kargopolov","3c5eef25-f335-4452-b449-5c0f2f7537af");
     }
 }
--- src/main/java/photo/security/WebSecurity.java
+++ src/main/java/photo/security/WebSecurity.java
@@ -1,24 +1,35 @@
 package photo.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
-@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 @EnableWebSecurity
-public class WebSecurity {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain configure(HttpSecurity http) throws Exception{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests( authz -> authz
-                        .requestMatchers(HttpMethod.GET, "users/check").hasAuthority("SCOPE_profile") //Web security will check if the jwt has profile scope for this endpoint after it has checked if the jwt is valid
-                        .anyRequest().authenticated())//all requests must come from authenticated users
-                .oauth2ResourceServer( oauth2 -> oauth2.jwt(jwt->{})); //specifies that this is a resource project that follow oauth2 rules and is expecting jwt token for security
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
-        return http.build();
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/users/check")
+                //.hasAuthority("SCOPE_profile")
+                .hasRole("developer")
+                //.hasAnyAuthority("ROLE_developer")
+                //.hasAnyRole("devleoper","user")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
+
 }
